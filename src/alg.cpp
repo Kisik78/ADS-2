b46aa6d03fa7c4abf40d90addfc9b83ef77ec7aa
Коньков Арсений <95272498+Kisik78@users.noreply.github.com>
2022-03-03 13:32:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -30,7 +30,7 @@ double calcItem(double x, uint16_t n) {
 double expn(double x, uint16_t count) {
   double uni = 0;
   for (int n = 0; n <= count; n++) {
-    uni += calcItem (x,n);
+    uni += calcItem(x,n);
   }
   return uni;
 }
@@ -38,7 +38,7 @@ double expn(double x, uint16_t count) {
 double sinn(double x, uint16_t count) {
   double uni = 0;
   while (count != 0) {
-        uni += pown (-1, count - 1) * calcItem (x, 2 * count - 1);
+        uni += pown(-1, count - 1) * calcItem(x, 2 * count - 1);
         count--;
   }
   return uni;
@@ -47,7 +47,7 @@ double sinn(double x, uint16_t count) {
 double cosn(double x, uint16_t count) {
   double uni = 0;
   for (uint16_t n = 1; n <= count; n++) {
-    uni += pown (-1, (n - 1)) * calcItem (x, (2 * n - 2));
+    uni += pown(-1, (n - 1)) * calcItem(x, (2 * n - 2));
   }
   return uni;
 }
